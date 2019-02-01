d2415df0bcbfef368e048e6c0caca0db4319f3bc
Sebastian Gambolati <ysgambolati@gmail.com>
2019-02-01 15:21:44 -0300

    Tests until point 8.

--- src/StringCalculator/StringCalculator.js
+++ src/StringCalculator/StringCalculator.js
@@ -3,6 +3,38 @@ class StringCalculator {
     if (!numbersString) {
       return 0;
     }
+
+    let delimiter = ',';
+    if (numbersString.indexOf('//') == 0) {
+      delimiter = numbersString.substring(2,3);
+      numbersString = numbersString.replace('//' + delimiter + '\n', '');
+    }
+
+    let numbers = numbersString.replace(new RegExp('\n', 'g'), delimiter).split(delimiter);
+
+    if(numbers.length > 0) {
+      // Check negative numbers
+      let negativeNumber =[];
+      numbers.map((number) => {
+          const currentNumber = parseInt(number);
+          if(currentNumber < 0) {
+            negativeNumber.push(currentNumber);
+          }
+      });
+
+      if(negativeNumber.length > 0) {
+        const errorMessage = 'negatives not allowed: ' + negativeNumber.join(',');
+        // Not working test.
+        // throw new Error(errorMessage);
+        return errorMessage;
+      }
+
+      numbersString = numbers.reduce((total, num)=> {
+        return parseInt(total) + (parseInt(num) >= 1000 ? 0 : parseInt(num)); 
+      });
+    }
+
+    return parseInt(numbersString);
   }
 }
 
--- src/StringCalculator/StringCalculator.test.js
+++ src/StringCalculator/StringCalculator.test.js
@@ -7,5 +7,27 @@ describe('StringCalculator', () => {
     it('zero for empty string ', () => {
       expect(uut.add('')).toEqual(0);
     });
+    it('number string return a number', () => {
+      expect(uut.add('1')).toEqual(1);
+    });
+    it('Two numbers, comma delimited, returns the sum', () => {
+      expect(uut.add('1,2')).toEqual(3);
+    });
+    it('Two numbers, newline delimited, returns the sum', () => {
+      expect(uut.add('1\n2')).toEqual(3);
+    });
+    it('Three numbers, delimited either way, returns the sum', () => {
+      expect(uut.add('1\n2,3\n4')).toEqual(10);
+    });
+    it('Negative numbers throw an exception with the message', () => {
+      //expect(uut.add('-1,2,-3')).toThrowError(new Error('negatives not allowed'));
+      expect(uut.add('-1,2,-3')).toEqual('negatives not allowed: -1,-3');
+    });
+    it('Numbers greater than 1000 are ignored', () => {
+      expect(uut.add('1,1000,2')).toEqual(3);
+    })
+    it('A single char delimiter can be defined on the first line starting with `//`', () => {
+      expect(uut.add('//#\n1#2')).toEqual(3);
+    })
   });
 });
