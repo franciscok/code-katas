31dc9d809dcff91bf7e058423172e1c9dca14c6d
maxenz <maximilianopoggio@gmail.com>
2019-02-15 15:54:23 -0300

    Kata number 2: Prime factors

--- src/PrimeFactors/PrimeFactors.js
+++ src/PrimeFactors/PrimeFactors.js
@@ -4,6 +4,16 @@
  */
 function generate(number) {
   const factors = [];
+    for(i = 2; i < number ; i++) {
+      let residual = number;
+      while( residual % i  === 0){
+          residual = residual / i;
+          const mult = factors.reduce((acc, elem) => { return elem * acc }, 1)
+          if(mult < number)
+            factors.push(i);
+      }
+    }
+  if (factors.length === 0 && number > 1) return [number];
   return factors;
 }
 
--- src/PrimeFactors/PrimeFactors.test.js
+++ src/PrimeFactors/PrimeFactors.test.js
@@ -2,8 +2,40 @@ const PrimeFactors = require('./PrimeFactors');
 
 describe('PrimeFactors', () => {
   describe('generate', () => {
-    it('transformation: null to constant', () => {
+    it('should return empty array when passing 1', () => {
       expect(PrimeFactors.generate(1)).toEqual([]);
+    });
+
+    it('should return ar array with 2', () => {
+      expect(PrimeFactors.generate(2)).toEqual([2]);
+    });
+
+    it('should return ar array with 3', () => {
+      expect(PrimeFactors.generate(3)).toEqual([3]);
+    });
+
+    it('should return ar array with 2,2', () => {
+      expect(PrimeFactors.generate(4)).toEqual([2,2]);
+    });
+
+    it('should return ar array with 5', () => {
+      expect(PrimeFactors.generate(5)).toEqual([5]);
+    });
+
+    it('should return ar array with 2,3', () => {
+      expect(PrimeFactors.generate(6)).toEqual([2,3]);
+    });
+
+    it('should return ar array with 7', () => {
+      expect(PrimeFactors.generate(7)).toEqual([7]);
+    });
+
+    it('should return ar array with 2,2,2', () => {
+      expect(PrimeFactors.generate(8)).toEqual([2,2,2]);
+    });
+
+    it('should return ar array with 3,3', () => {
+      expect(PrimeFactors.generate(9)).toEqual([3,3]);
     });
   });
 });
