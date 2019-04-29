6ba4347d12e989422fc2260136f2679bb98df4e5
Sebastian Gambolati <ysgambolati@gmail.com>
2019-04-29 14:26:26 -0300

    Tests created.

--- src/Greeter/Greeter.js
+++ src/Greeter/Greeter.js
@@ -1,5 +1,46 @@
-import _ from 'lodash';
+const _ = require('lodash');
 
 class Greeter {
+    
+    constructor(name, time) {
+        this.name = name;
+        this.time = null;
 
+        if (time) {
+            this.time = time;
+        }
+    }
+
+    normalizer(value) {
+        return _.startCase(_.toLower(value.trim()));
+    }
+    getGreetingMessage() {
+        const currentTime = this.time;
+        const hour = currentTime.getHours();
+        const minute = currentTime.getMinutes();
+
+        if(hour >= 6 && (hour < 12 || hour == 12 && minute == 0 )) {
+            return 'Good morning';
+        }
+        if (hour >= 18 && (hour < 22 || hour == 22 && minute == 0)) {
+            return 'Good evening';
+        }
+        if (hour < 6 || hour >= 22 ) {
+            return 'Good night';
+        }
+
+        return null;
+    }
+
+    greet() {
+        const value = this.normalizer(this.name);
+        let greeting = 'Hello';
+        if (this.time) {
+            greeting = this.getGreetingMessage();
+        }
+        console.log('Executing greet.');
+        return `${greeting} ${value}`;
+    }
 }
+
+module.exports = Greeter;
--- src/Greeter/Greeter.test.js
+++ src/Greeter/Greeter.test.js
@@ -1,4 +1,41 @@
-import Greeter from './Greeter'
+const  Greeter = require('./Greeter');
 
 describe('Greeter', () => {
+    it('Should return saludation', () => {
+        const greeter = new Greeter('Baburro');
+        expect(greeter.greet()).toEqual('Hello Baburro');
+    })
+    it('Should trim the input', () => {
+        const greeter = new Greeter('  Baburro  ');
+        expect(greeter.greet()).toEqual('Hello Baburro');
+    })
+
+    it('capitalizes the first letter of the name', () => {
+        const greeter = new Greeter('BABURRO');
+        expect(greeter.greet()).toEqual('Hello Baburro');
+    })
+
+    it('returns Good morning when the time is 06:00-12:00', () => {
+        const greeter = new Greeter('BABURRO', new Date(2019, 4, 29, 11, 33));
+        expect(greeter.greet()).toEqual('Good morning Baburro');
+    })
+
+    it('Returns Good evening when the time is 18:00-22:00', () => {
+        const greeter = new Greeter('BABURRO', new Date(2019, 4, 29, 19, 33));
+        expect(greeter.greet()).toEqual('Good evening Baburro');
+    })
+
+    it('Returns Good night when the time is 22:00-06:00', () => {
+        const greeter = new Greeter('BABURRO', new Date(2019, 4, 29, 4, 33));
+        expect(greeter.greet()).toEqual('Good night Baburro');
+    })
+
+    it ('Should logs into console each time it is called', () => {
+        global.console.log = jest.fn();
+
+        const greeter = new Greeter('BABURRO');
+        greeter.greet();
+
+        expect(console.log).toBeCalled();
+    })
 });
