ddfeb786a1167662d418613d7d986824e50ff524
franciscokala <francisco.med@gmail.com>
2019-04-29 15:00:45 -0300

    Greeter Kata

--- src/Greeter/Greeter.js
+++ src/Greeter/Greeter.js
@@ -1,5 +1,26 @@
-import _ from 'lodash';
+const _ = require('lodash');
 
 class Greeter {
+    constructor(date){
+        this.dateConst = date;
+    }
 
+    greet(name) {
+        let message = 'Hello';
+
+        if(this.dateConst) {
+            const dateTo = new Date(this.dateConst);
+            if (dateTo.getHours() >= 6 && dateTo.getHours() <=12 ) {
+                message = 'Good Morning';
+            } else if (dateTo.getHours() > 12 && dateTo.getHours() <=22) {
+                message = 'Good Evening';
+            } else {
+                message = 'Good Night';
+            }
+        }
+        console.log('Executing method');
+        return `${message} ${_.startCase(_.capitalize(name.trim()))}`;
+    }
 }
+
+module.exports = Greeter;
--- src/Greeter/Greeter.test.js
+++ src/Greeter/Greeter.test.js
@@ -1,4 +1,56 @@
-import Greeter from './Greeter'
+const Greeter = require('./Greeter');
 
 describe('Greeter', () => {
+
+    describe('greet', () => {
+        const greeter = new Greeter();
+
+        it('should return hello <name>', () => {
+            expect(greeter.greet('pepe')).toEqual('Hello Pepe');
+        });
+
+        it('should return hello <name> trimmed', () => {
+            expect(greeter.greet(' pepe ')).toEqual('Hello Pepe');
+        });
+
+        it('should return hello <name> capitalized', () => {
+            expect(greeter.greet('pepe')).toEqual('Hello Pepe');
+        });
+    });
+    describe('greet with date morning', () => {
+        const today = new Date();
+        const greeter = new Greeter(
+            new Date(today.getFullYear(), today.getMonth(), today.getDate(), 7, 0, 0)
+        );
+
+        it('should return hello <name> and Good Morning', () => {
+            expect(greeter.greet('pepe')).toEqual('Good Morning Pepe');
+        });
+    });
+    describe('greet with date evening', () => {
+        const today = new Date();
+        const greeter = new Greeter(
+            new Date(today.getFullYear(), today.getMonth(), today.getDate(), 14, 0, 0)
+        );
+
+        it('should return hello <name> and Good Evening', () => {
+            expect(greeter.greet('pepe')).toEqual('Good Evening Pepe');
+        });
+    });
+
+    describe('greet with date night', () => {
+        const today = new Date();
+        const greeter = new Greeter(
+            new Date(today.getFullYear(), today.getMonth(), today.getDate(), 23, 0, 0)
+        );
+
+        it('should return hello <name> and Good Night', () => {
+            expect(greeter.greet('pepe')).toEqual('Good Night Pepe');
+        });
+        it('should console.log hello <name> and Good Night', () => {
+            global.console.log = jest.fn();
+            greeter.greet('pepe');
+            expect(console.log).toBeCalled();
+        });
+    });
 });
